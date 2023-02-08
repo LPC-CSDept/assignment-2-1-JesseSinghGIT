ac8c0f3387d043c48d5f4b6565232878c4745e9c
JesseSinghGIT <123704475+JesseSinghGIT@users.noreply.github.com>
2023-02-08 10:19:25 +0000

    program

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+
+    # Input
+    numM = int(input('Enter the number of males: ')) # input() retuns string value. Need to change it to INT variable. 
+    # num = int(numM)
+    numF = int(input('Enter the number of females: '))
+    
+    # Calculation
+    # total = numM + numF
+    percM = numM / (numM + numF) * 100
+    percF = numF / (numM + numF) * 100
+
+    print('The total number of students: ', '\t   ',(numM + numF))
+    print ( 'Number of males and females' , '\t   ', numM, numF)
+    # print ( 'Percentage of males and females', percM, percF)
+    print (f'Percentage of males and females :  {percM: .2f} % {percF: .2f}%')
+
     pass
 
 
